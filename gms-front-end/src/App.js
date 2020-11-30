f37acf2b9c8f734c4a555f00cf7a822fbe845bfa
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2020-11-30 19:12:34 +0100

    added ListProductComponent

--- gms-front-end/src/App.js
+++ gms-front-end/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListProductComponent from './components/ListProductComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World..!</h1>
-      </header>
+     <ListProductComponent />
     </div>
   );
 }
---
+++ gms-front-end/src/components/ListProductComponent.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class ListProductComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            products:[]
+        }
+    }
+    render() {
+        return (
+            <div>
+               <h2 className="text-center">Products List</h2>
+               <br/>
+               <div className="row">
+                   <table className="table table-striped table-bordered">
+                       <thead>
+                           <tr>
+                               <th>Product Name</th>
+                               <th>Product Category</th>
+                               <th>Product Quantity</th>
+                               <th>Actions</th>
+                           </tr>
+                       </thead>
+
+                       <tbody>
+                           {
+                               this.state.products.map(
+                                   product => 
+                                   <tr key={product.id}>
+                                       <td>{product.productName}</td>
+                                       <td>{product.productCategory}</td>
+                                       <td>{product.productQuantity}</td>                                       
+                                   </tr>
+                               )
+                           }
+                       </tbody>
+                   </table>
+               </div>
+            </div>
+        );
+    }
+}
+
+export default ListProductComponent;
